6635851556ffc8707d02080edf2087056214263b
Alex Radnaev <alexander.radnaev@gmail.com>
2018-10-23 22:50:10 -0600

    hooked ETA all link

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -8,52 +8,57 @@ import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthService {
-	public token: string;
-  service_api_end_point: string;
-	public users: any;
-  @Output() getLoggedIn: EventEmitter<boolean> = new EventEmitter(); 
-	constructor(private http:Http, private router: Router) {
-    this.service_api_end_point = environment.apiUrl
-  }
+
+    public token: string;
+    // service_api_end_point: string;
+    public users: any;
+
+    @Output() getLoggedIn: EventEmitter<boolean> = new EventEmitter();
+
+    constructor(private http:Http, private router: Router) {
+        // this.service_api_end_point = 
+    }
+
+    // var loggedIn = JSON.parse(localStorage.getItem('currentUser'))
+    // this.token = loggedIn && loggedIn.token;
 
 
+    login(username: string, password: string) {
+        let headers = new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        });
+        let options = new RequestOptions({
+            headers: headers
+        });
 
-	login(username: string, password: string) {
-		let headers = new Headers({
-  		'Accept': 'application/json',
-  		'Content-Type': 'application/json'
-		});
-		let options = new RequestOptions({
-  		headers: headers
-		});
+        return this.http.post(environment.apiUrl + 'get-token/', JSON.stringify({ username: username, password: password }), options)
+                .pipe(map((response: Response) => {
+                    
+          
+                let user = response.json();
+              if (user && user.token) {
+                localStorage.setItem('currentUser', JSON.stringify(user));
+                localStorage.setItem('username', username);
+                this.token = user.token
+                this.getLoggedIn.emit(true);
+                return true;
 
-    return this.http.post(this.service_api_end_point + 'get-token/', JSON.stringify({ username: username, password: password }), options)
-			.pipe(map((response: Response) => {
-				
-      
-      		let user = response.json();
-          if (user && user.token) {
-            localStorage.setItem('currentUser', JSON.stringify(user));
-            localStorage.setItem('username', username);
-            this.getLoggedIn.emit(true);
-            return true;
+              }
+              else {
+                 this.getLoggedIn.emit(false);
+                return false;
 
-          }
-          else {
-             this.getLoggedIn.emit(false);
-            return false;
+              }
+        }));
 
-          }
-    }));
+    }
 
-
-	}
-
-	logout() {
-		this.token = null;
-		localStorage.removeItem('currentUser');
+    logout() {
+        this.token = null;
+        localStorage.removeItem('currentUser');
     localStorage.removeItem('username');
     this.getLoggedIn.emit(false);
     this.router.navigate(['/']);
-	}
+    }
 }
--- src/app/services/jira.service.ts
+++ src/app/services/jira.service.ts
@@ -5,17 +5,17 @@ import { environment } from '../../environments/environment';
 
 @Injectable()
 export class JiraService {
-	private service_api_end_point: string;
-	private token: string;
+    private service_api_end_point: string;
+    private token: string;
   constructor(private http: Http) {
-  	this.service_api_end_point = environment.apiUrl;
+    this.service_api_end_point = environment.apiUrl;
   }
 
   jira(owner: string, team_name: string, email: string, password: string) {
-  	var loggedIn = JSON.parse(localStorage.getItem('currentUser'))
+    var loggedIn = JSON.parse(localStorage.getItem('currentUser'))
     this.token = loggedIn && loggedIn.token;
     console.log("token is " + this.token)
-  	let type = 'JI';
+    let type = 'JI';
 // todo: check type from url         
 //         if 'atlassian.net' in self.server:
 //             self.TMS_type = TMSTypes.JIRA
@@ -23,7 +23,7 @@ export class JiraService {
 //             raise NameError('cannot recognize Task Management System \
 // based on server name {}'.format(server))
 
-  	let headers = new Headers({
+    let headers = new Headers({
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     });
@@ -39,32 +39,32 @@ export class JiraService {
           if (String(response.status) == "201")
             return true;
           else 
-          	return false;
+            return false;
         }));
   }
 
   get_tms() {
-  	var loggedIn = JSON.parse(localStorage.getItem('currentUser'));
-  	this.token = loggedIn && loggedIn.token;
-  	let headers = new Headers({
-  		'Accept': 'application/json',
-  		'Content-Type': 'application/json'
-  	});
-  	headers.append('Authorization', `Token ${this.token}`);
-  	let options = new RequestOptions({
-  		headers: headers
-  	});
+    var loggedIn = JSON.parse(localStorage.getItem('currentUser'));
+    this.token = loggedIn && loggedIn.token;
+    let headers = new Headers({
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    });
+    headers.append('Authorization', `Token ${this.token}`);
+    let options = new RequestOptions({
+        headers: headers
+    });
 
-  	return this.http.get(this.service_api_end_point + 'tms/', options)
-  		.pipe(map((response: Response) => {
-  				let res = response.json();
-  				if (res.length == 0) {
-  					throw new Error('user does not have TMS accounts');
-  				}
-  				else {
+    return this.http.get(this.service_api_end_point + 'tms/', options)
+        .pipe(map((response: Response) => {
+                let res = response.json();
+                if (res.length == 0) {
+                    throw new Error('user does not have TMS accounts');
+                }
+                else {
                     console.log('number of TMS accounts found: ' + res.length)
                     return res.length;
-  				}
-  		}))
+                }
+        }))
   }
 }

8f1b2579d49c71ab4828ecaae1a4011a8124fbec
misha <misha.behey@gmail.com>
2019-08-04 20:45:39 -0500

    exception handling for jira adding page

--- src/app/components/jira-credentials/jira-credentials.component.ts
+++ src/app/components/jira-credentials/jira-credentials.component.ts
@@ -4,6 +4,8 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { Title } from '@angular/platform-browser';
 import { JiraService } from '../../services/jira.service';
 import { Router } from '@angular/router';
+//import { }
+
 @Component({
   selector: 'app-jira-credentials',
   templateUrl: './jira-credentials.component.html',
@@ -16,8 +18,11 @@ export class JiraCredentialsComponent implements OnInit {
   team: string;
   type: 'JIRA';
   error: boolean;
+  error_status: string;
+  error_status_text: string;
   loading: boolean;
   error_message: string;
+  attempts_to_connect: number;
   constructor(
     public dialog: MatDialog,
     private titleService: Title,
@@ -32,6 +37,8 @@ export class JiraCredentialsComponent implements OnInit {
     this.titleService.setTitle('JIRA Credentials');
     this.error = false;
     this.loading = false;
+    this.attempts_to_connect = 3;
+    this.error_status_text = "Description temporary unavailable";
   }
 
   openDialog(): void {
@@ -44,6 +51,7 @@ export class JiraCredentialsComponent implements OnInit {
   add_tms_via_service() {
     this.loading = true;
     this.error = false;
+    //this.attempt_to_connect = this.attempt_to_connect + 1;
     this.jiraService.add_tms(
         this.username,
         this.model.jira_url,
@@ -58,7 +66,18 @@ export class JiraCredentialsComponent implements OnInit {
         this.loading = false;
         this.error = true;
         this.error_message = this.jiraService.parse_error(error);
-      }
+        
+        // this type of parsing is needed because server always returns 
+        // error 500 so there is no other way to differentiate
+        // for now. 
+        this.error_status = this.error_message.split(' ')[13];
+        if (this.error_status == "404")
+          this.error_status_text = "JIRA Page not found.";
+        else if (this.error_status == "401")
+          this.error_status_text = "JIRA e-mail or password incorrect.";
+        else
+          this.error_status_text = "Internal Server Error.";
+        }
     );
   }
 }

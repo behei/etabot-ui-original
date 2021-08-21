9c3c4b94d6ef78759d06a05b3de2e132fe64e2e2
ZachElkins <zach.elkins@gmil.com>
2021-08-21 12:59:57 -0600

    summary values update when selecting user(s)

--- src/app/components/project-card/project-summary/project-summary.component.ts
+++ src/app/components/project-card/project-summary/project-summary.component.ts
@@ -15,70 +15,28 @@ export class ProjectSummaryComponent implements OnInit {
   fullReport: String;
   dueDatesSummary: Object;
   sprintSummary: Object;
-  team: any[];
+  teamSummary: any[];
+  summaryValues: {dueDate: Object, sprint: Object};
 
   constructor(
     private rd: Renderer2
   ) { }
 
   ngOnInit() {
-    // this.keys = Object.keys(this.project);
-    this.fullReport = this.project.settings.reports
+    this.fullReport = this.project.settings.reports;
     this.dueDatesSummary = this.project.settings.hierarchical_report.due_dates_stats.counts;
     this.sprintSummary = this.project.settings.hierarchical_report.sprint_stats.counts;
-    this.team = [
-      {
-        name: 'Chad Lewis',
-      },
-      {
-        name: 'Alex Radnaev',
-        avatar: 'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/557058:5e8ff89e-5f83-48cd-b787-fd0bd83ce3c2/eb3fdfb5-0e51-4995-9d7a-11df45e07e5b/48'
-      },
-      {
-        name: 'Zach Elkins',
-        avatar: 'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/604192c2c58c7200714eeda8/c5495077-983e-45fd-99cf-23a860fefe50/48'
-      },
-      {
-        name: 'Chad Lewis',
-      },
-      {
-        name: 'Alex Radnaev',
-        avatar: 'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/557058:5e8ff89e-5f83-48cd-b787-fd0bd83ce3c2/eb3fdfb5-0e51-4995-9d7a-11df45e07e5b/48'
-      },
-      {
-        name: 'Zach Elkins',
-        avatar: 'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/604192c2c58c7200714eeda8/c5495077-983e-45fd-99cf-23a860fefe50/48'
-      },
-      {
-        name: 'Chad Lewis',
-      },
-      {
-        name: 'Alex Radnaev',
-        avatar: 'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/557058:5e8ff89e-5f83-48cd-b787-fd0bd83ce3c2/eb3fdfb5-0e51-4995-9d7a-11df45e07e5b/48'
-      },
-      {
-        name: 'Zach Elkins',
-        avatar: 'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/604192c2c58c7200714eeda8/c5495077-983e-45fd-99cf-23a860fefe50/48'
-      },
-      {
-        name: 'Chad Lewis',
-      },
-      {
-        name: 'Alex Radnaev',
-        avatar: 'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/557058:5e8ff89e-5f83-48cd-b787-fd0bd83ce3c2/eb3fdfb5-0e51-4995-9d7a-11df45e07e5b/48'
-      },
-      {
-        name: 'Zach Elkins',
-        avatar: 'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net/604192c2c58c7200714eeda8/c5495077-983e-45fd-99cf-23a860fefe50/48'
-      }
-    ];
+    this.teamSummary = this.project.settings.hierarchical_report.children;
 
-    // this.team = this.team.sort((a, b) => {
-    //   return compareNamesAlphabetical(a.name, b.name);
-    // });
+    // console.log("TEAM", this.teamSummary);
+    // console.log("PROJECT SUMMARY ", this.project.settings);
+    // console.log("PROJECT ", this.project);
+    // console.log("DUE DATE SUMMARY", this.dueDatesSummary);
 
-    console.log("TEAM", this.team);
-    console.log("PROJECT SUMMARY ", this.project.settings);
+    this.summaryValues = {
+      dueDate: this.dueDatesSummary,
+      sprint: this.sprintSummary,
+    }
   }
 
   ngAfterViewInit() {
@@ -89,16 +47,78 @@ export class ProjectSummaryComponent implements OnInit {
         } else {
           this.rd.addClass(element, 'selected-user');
         }
+        this.updateCharts()
       });
     });
   }
 
   more() {
-    console.log("DISPLAY SOME MORE")
+    console.log("DISPLAY SOME MORE INFO");
   }
 
   openSelect() {
     this.extraUserSelect.open()
+  }
+
+  selectUser(evt, target, child) {
+    if (!evt.value.length) {
+      this.rd.removeClass(target, 'selected-user');
+    } else if (!target.classList.contains('selected-user')) {
+      this.rd.addClass(target, 'selected-user');
+    }
+    this.updateCharts()
+  }
+
+  getSelectedUserIds() {
+    let ids = []
+    let elements = this.userSelect.nativeElement.querySelectorAll('.user')
+    for (let i = 0; i < elements.length; i++) {
+      if (elements[i].classList.contains('selected-user')) {
+        ids.push(parseInt(elements[i].getAttribute('value')))
+      }
+    }
+    elements = this.extraUserSelect.options.toArray()
+
+    for (let i = 0; i < elements.length; i++) {
+      if (elements[i].selected) {
+        ids.push(elements[i].value);
+      }
+    }
+
+    return ids;
+  }
+
+  sumObjects(objs) {
+    let sum = {}
+
+    objs.forEach(user => {
+      for (let [key, value] of Object.entries(user)) {
+        if (sum[key]) {
+          sum[key] += value
+        } else {
+          sum[key] = value
+        }
+      }
+    })
+
+    return sum
+  }
+
+  updateCharts() {
+    let selectedIds = this.getSelectedUserIds()
+    let selectedUsers = this.teamSummary.filter(user => selectedIds.includes(user.id))
+    
+    if (selectedUsers.length) {
+      this.summaryValues = {
+        dueDate: this.sumObjects(selectedUsers.map(user => user.due_dates_stats.counts)),
+        sprint: this.sumObjects(selectedUsers.map(user => user.sprint_stats.counts))
+      }
+    } else {
+      this.summaryValues = {
+        dueDate: this.dueDatesSummary,
+        sprint: this.sprintSummary,
+      }
+    }
   }
 }
 
